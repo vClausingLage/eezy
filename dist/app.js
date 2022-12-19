53a768dcd6eaeff698f63b1ccdbad5696ea2fc63
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2022-12-19 13:57:00 +0100

    server

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const sequelize_1 = require("sequelize");
-dotenv_1.default.config();
-const app = (0, express_1.default)();
+import express from 'express';
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+dotenv.config();
+const app = express();
 const port = process.env.PORT;
 const db = process.env.DB;
 const user = process.env.USER;
@@ -15,16 +10,17 @@ const pass = process.env.PASS;
 app.get('/', (req, res) => {
     res.send('Läuft');
 });
-const sequelize = new sequelize_1.Sequelize(db, user, pass, {
+const sequelize = new Sequelize(db, user, pass, {
     host: 'localhost:8081',
     dialect: 'mysql'
 });
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// } catch (error) {
-//   console.error('Unable to connect to the database:', error);
-// }
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+}
+catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
--- dist/unit_calculators.js
+++ dist/unit_calculators.js
@@ -1,11 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.gallonsToLiters = exports.kilometersToKts = void 0;
-function kilometersToKts(input) {
+export function kilometersToKts(input) {
     return input * 0.54;
 }
-exports.kilometersToKts = kilometersToKts;
-function gallonsToLiters(input) {
+export function gallonsToLiters(input) {
     return input * 3.785;
 }
-exports.gallonsToLiters = gallonsToLiters;
