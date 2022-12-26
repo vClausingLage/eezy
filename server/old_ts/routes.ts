6b00c50919fcda930167479914149f389109013b
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2022-12-26 19:59:59 +0100

    server env

--- server/old_ts/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router, Request, Response } from 'express'
-import { getAircraft } from './controller.js'
-
-const routes = Router()
-
-routes.post('/aircraft', getAircraft)
-
-export default routes
--- server/old_ts/unit_calculators.ts
+++
@@ -1,7 +0,0 @@
-export function kilometersToKts(input:number) {
-  return input * 0.54;
-}
-
-export function gallonsToLiters(input:number) {
-  return input * 3.785;
-}
