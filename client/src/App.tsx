5a58de8d8d3833d6b4c26fcccc45d6e734bb1530
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2022-12-30 06:45:33 +0100

    server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      <h1>EEzy Flight Computer</h1>
+      <h1>EEzy Flight Planner</h1>
       <Form />
     </div>
   )
--- server/dist/services/controllers.js
+++ server/dist/services/controllers.js
@@ -12,6 +12,12 @@ export async function query(req, res) {
         console.error('error');
     }
 }
+// export async function create(req: Request, res: Response) {
+//   const userAircraft = await userAircraft.create({
+//     firstName: "Jane", 
+//     lastName: "Doe"
+//   });
+// }
 // https://stackoverflow.com/questions/45859745/where-should-i-write-queries-in-model-or-controller-sequelize
 // https://stackoverflow.com/questions/62685728/use-model-in-sequelize-from-a-controller
 // localhost:4000/api/aircraft
