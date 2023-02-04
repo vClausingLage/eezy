8f9bb61897d6f1ff1e4cf3adc9d90a65b55ea445
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-02-04 10:06:58 +0100

    metar API classes

---
+++ server/MetarAPI/metar.ts
@@ -0,0 +1,15 @@
+// import {
+//   prepareMetar,
+//   checkMetarIntegr,
+//   reduceTempo,
+//   maptoMetarObj,
+// } from "./Metar/metar-regex";
+// import { precipitation, getFlightRules } from "./Metar/metar-ui-helper";
+// import { IMetar, IFlightRule } from "./Metar/assets/IMetar";
+
+import { prepareMetar, checkMetarIntegr, reduceTempo, maptoMetarObj } from "./helper/metar-regex.js";
+import { precipitation, getFlightRules } from "./helper/metar-ui-helper.js";
+import { IMetar, IFlightRule } from "./assets/IMetar.js";
+
+
+// send logs
