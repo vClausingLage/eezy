3a06d113b2af8bee40935aab0b3268eb7d6a9845
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2023-02-07 09:00:35 +0100

    API removed outdated functions

--- server/MetarAPI/metar.ts
+++ server/MetarAPI/metar.ts
@@ -7,10 +7,6 @@
 // import { precipitation, getFlightRules } from "./Metar/metar-ui-helper";
 // import { IMetar, IFlightRule } from "./Metar/assets/IMetar";
 
-import { prepareMetar, checkMetarIntegr, reduceTempo, maptoMetarObj } from "./helper/metar-regex.js";
-import { precipitation, getFlightRules } from "./helper/metar-ui-helper.js";
-import { IMetar, IFlightRule } from "./assets/IMetar.js";
-
 
 // send logs
 
