e486ed9905ae1dcb262da035f29a5595f35bfcff
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-02-14 18:58:23 +0100

    app.ts to prod

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import path from 'path'
+import {fileURLToPath} from 'url';
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { ac_router } from './routes/aircraft.js'
 import { awc_router } from './routes/awc_routes.js'
 const app = express();
 dotenv.config()
-const port = process.env.PORT;
+const port =  4000; // process.env.PORT ||
 
 app.use(express.json());
 app.use(
@@ -19,6 +20,8 @@ app.use(cors())
 // app.use("/aircraft", ac_router);
 app.use('/api', awc_router)
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
