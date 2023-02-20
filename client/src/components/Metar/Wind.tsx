7bcf541af647f80aa2b72f301680191bd39a277d
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-02-20 12:48:34 +0100

    SVG Wind with RWY

--- client/src/components/Metar/Wind.tsx
+++ client/src/components/Metar/Wind.tsx
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react";
 
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Grid, Typography, Button } from "@mui/material";
 import { IWind } from "./IMetar";
 
 function Wind(props: IWind) {
   const [runwayDir, setRunwayDir] = useState(0);
+  const opacityNorth =
+    typeof props.direction === "number" &&
+    (props.direction > 330 || props.direction < 30)
+      ? "50%"
+      : "100%";
 
   useEffect(() => {
-    setRunwayDir((parseInt(props.runways[0].he_ident.slice(0, 2)) - 90) * 100); // ! change SVG
+    setRunwayDir(parseInt(props.runways[0].he_ident.slice(0, 2)) * 10);
   }, []);
+  function setRunwayDirection(input: string) {
+    let degrees = parseInt(input.slice(0, 2)) * 10;
+    setRunwayDir(degrees);
+  }
 
   const arrow180 = (
     <g
-      id="Arrow Wind"
+      id="Arrow 2"
       style={{
         transformBox: "fill-box",
         transformOrigin: "center",
@@ -21,34 +30,28 @@ function Wind(props: IWind) {
     >
       <circle cx="200" cy="200" r="200" />
       <path
-        d="M197.879 88.1213C199.05 89.2929 200.95 89.2929 202.121 88.1213L221.213 69.0294C222.385 67.8579 222.385 65.9584 221.213 64.7868C220.042 63.6152 218.142 63.6152 216.971 64.7868L200 81.7574L183.029 64.7868C181.858 63.6152 179.958 63.6152 178.787 64.7868C177.615 65.9584 177.615 67.8579 178.787 69.0294L197.879 88.1213ZM197 1V86H203V1L197 1Z"
-        fill="#660000"
+        d="M198.939 73.0607C199.525 73.6464 200.475 73.6464 201.061 73.0607L210.607 63.5147C211.192 62.9289 211.192 61.9792 210.607 61.3934C210.021 60.8076 209.071 60.8076 208.485 61.3934L200 69.8787L191.515 61.3934C190.929 60.8076 189.979 60.8076 189.393 61.3934C188.808 61.9792 188.808 62.9289 189.393 63.5147L198.939 73.0607ZM198.5 0V72H201.5V0L198.5 0Z"
+        fill="#C65F00"
       />
       <text
-        transform="matrix(0 -1 1 0 208 59)"
-        fill="#660000"
+        id="306&#194;&#176; 20kts"
+        transform="matrix(0 -1 1 0 180 64)"
+        fill="#C65F00"
         fontFamily="Roboto"
         fontSize="18"
       >
-        <tspan x="1.40194" y="16.1523">
-          {props.speed} kts
-        </tspan>
-      </text>
-      <text
-        transform="matrix(0 -1 1 0 172 59)"
-        fill="#660000"
-        fontFamily="Roboto"
-        fontSize="18"
-      >
-        <tspan x="7.36092" y="16.1523">
+        <tspan x="13.4463" y="15.6523">
           {props.direction}°
+        </tspan>
+        <tspan x="9.71973" y="36.6523">
+          {props.speed}kts
         </tspan>
       </text>
     </g>
   );
   const arrow360 = (
     <g
-      id="Arrow Wind"
+      id="Arrow 1"
       style={{
         transformBox: "fill-box",
         transformOrigin: "center",
@@ -57,164 +60,236 @@ function Wind(props: IWind) {
     >
       <circle cx="200" cy="200" r="200" />
       <path
-        d="M197.879 88.1213C199.05 89.2929 200.95 89.2929 202.121 88.1213L221.213 69.0294C222.385 67.8579 222.385 65.9584 221.213 64.7868C220.042 63.6152 218.142 63.6152 216.971 64.7868L200 81.7574L183.029 64.7868C181.858 63.6152 179.958 63.6152 178.787 64.7868C177.615 65.9584 177.615 67.8579 178.787 69.0294L197.879 88.1213ZM197 1V86H203V1L197 1Z"
-        fill="#660000"
+        d="M198.939 73.0607C199.525 73.6464 200.475 73.6464 201.061 73.0607L210.607 63.5147C211.192 62.9289 211.192 61.9792 210.607 61.3934C210.021 60.8076 209.071 60.8076 208.485 61.3934L200 69.8787L191.515 61.3934C190.929 60.8076 189.979 60.8076 189.393 61.3934C188.808 61.9792 188.808 62.9289 189.393 63.5147L198.939 73.0607ZM198.5 0V72H201.5V0L198.5 0Z"
+        fill="#C65F00"
       />
       <text
-        transform="matrix(0 1 -1 0 225 8)"
-        fill="#660000"
+        id="306&#194;&#176; 20kts_2"
+        transform="matrix(0 1 -1 0 220 0)"
+        fill="#C65F00"
         fontFamily="Roboto"
         fontSize="18"
       >
-        <tspan x="1.40194" y="16.1523">
-          {props.speed} kts
-        </tspan>
-      </text>
-      <text
-        transform="matrix(0 1 -1 0 193 8)"
-        fill="#660000"
-        fontFamily="Roboto"
-        fontSize="18"
-      >
-        <tspan x="7.36092" y="16.1523">
+        <tspan x="13.4463" y="15.6523">
           {props.direction}°
+        </tspan>
+        <tspan x="9.71973" y="36.6523">
+          {props.speed}kts
         </tspan>
       </text>
     </g>
   );
 
-  function setRunwayDirection(input: string) {
-    let degrees = parseInt(input.slice(0, 2)) * 10 - 90; // ! change SVG
-    setRunwayDir(degrees);
-  }
-  const opacityNorth =
-    typeof props.direction === "number" &&
-    (props.direction > 330 || props.direction < 30)
-      ? "50%"
-      : "100%";
   const compass = (
-    <svg
-      width="400"
-      height="400"
-      viewBox="0 0 400 400"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="400" height="400" fill="white" />
-      <g id="Compass">
-        <path
-          d="M323 199.5C323 267.155 268.155 322 200.5 322C132.845 322 78 267.155 78 199.5C78 131.845 132.845 77 200.5 77C268.155 77 323 131.845 323 199.5Z"
-          fill="#D9D9D9"
-        />
-        <circle cx="83" cy="200" r="5" fill="#182849" />
-        <circle cx="318" cy="200" r="5" fill="#182849" />
-        <circle cx="200" cy="317" r="5" fill="#182849" />
-        <circle
-          cx="200"
-          cy="97"
-          r="20"
-          fill="#182849"
-          id="north"
-          opacity={opacityNorth}
-        />
-        <text fill="white" fontFamily="Jost" fontSize="24">
-          <tspan x="191.042" y="105.334">
-            N
-          </tspan>
-        </text>
-      </g>
-      {typeof props.direction === "number" &&
-      props.direction > 0 &&
-      props.direction <= 180
-        ? arrow180
-        : arrow360}
-      <g
-        id="Runway"
-        style={{
-          transformBox: "fill-box",
-          transformOrigin: "center",
-          transform: `rotate(${runwayDir}deg)`,
+    <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+      <svg
+        width="400"
+        height="400"
+        viewBox="0 0 400 400"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="Compass">
+          <rect width="400" height="400" fill="white" fillOpacity="0.19" />
+          <path
+            id="Circle"
+            d="M323 199.5C323 267.155 268.155 322 200.5 322C132.845 322 78 267.155 78 199.5C78 131.845 132.845 77 200.5 77C268.155 77 323 131.845 323 199.5Z"
+            fill="#D9D9D9"
+          />
+          <circle id="West" cx="83" cy="200" r="5" fill="#182849" />
+          <circle id="East" cx="318" cy="200" r="5" fill="#182849" />
+          <circle id="South" cx="200" cy="317" r="5" fill="#182849" />
+          <g id="North">
+            <circle id="North_2" cx="200" cy="97" r="20" fill="#182849" />
+            <text id="N" fill="#D9D9D9" fontFamily="Jost" fontSize="24">
+              <tspan x="191.042" y="105.334">
+                N
+              </tspan>
+            </text>
+          </g>
+          <g
+            id="Runway"
+            style={{
+              transformBox: "fill-box",
+              transformOrigin: "center",
+              transform: `rotate(${runwayDir}deg)`,
+            }}
+          >
+            <rect
+              id="Rectangle 11"
+              x="183"
+              y="112"
+              width="35"
+              height="180"
+              fill="#1E1E1E"
+            />
+            <rect
+              id="Rectangle 12"
+              x="188"
+              y="118"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 13"
+              x="198"
+              y="118"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 18"
+              x="198"
+              y="192"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 19"
+              x="198"
+              y="155"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 20"
+              x="198"
+              y="229"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 17"
+              x="198"
+              y="267"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 14"
+              x="208"
+              y="118"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 15"
+              x="188"
+              y="267"
+              width="5"
+              height="18"
+              fill="white"
+            />
+            <rect
+              id="Rectangle 16"
+              x="208"
+              y="267"
+              width="5"
+              height="18"
+              fill="white"
+            />
+          </g>
+          {typeof props.direction === "number" &&
+          props.direction > 0 &&
+          props.direction <= 180
+            ? arrow180
+            : arrow360}
+        </g>
+      </svg>
+      <Box
+        sx={{
+          textAlign: "right",
+          color: "white",
+          border: 1,
+          borderColor: "primary.main",
+          borderRadius: 2,
+          overflow: "hidden",
+          ml: 1,
         }}
       >
-        <rect x="100" y="182" width="200" height="35" fill="#1E1E1E" />
-        <rect x="105" y="197" width="20" height="5" fill="white" />
-        <rect x="190" y="197" width="20" height="5" fill="white" />
-        <rect x="232" y="197" width="20" height="5" fill="white" />
-        <rect x="232" y="197" width="20" height="5" fill="white" />
-        <rect x="147" y="197" width="20" height="5" fill="white" />
-        <rect x="105" y="187" width="20" height="5" fill="white" />
-        <rect x="105" y="207" width="20" height="5" fill="white" />
-        <rect x="275" y="197" width="20" height="5" fill="white" />
-        <rect x="275" y="187" width="20" height="5" fill="white" />
-        <rect x="275" y="207" width="20" height="5" fill="white" />
-      </g>
-    </svg>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            opacity: "60%",
+            pl: 2,
+            pr: 2,
+            pt: 0.4,
+            pb: 0.4,
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}>
+            {props.direction}°
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            opacity: "60%",
+            pl: 2,
+            pr: 2,
+            pt: 0.4,
+            pb: 0.4,
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}>
+            {props.speed} {props.unit}
+          </Typography>
+        </Box>
+        {props.gusts && (
+          <Box
+            sx={{
+              backgroundColor: "warning.main",
+              opacity: "60%",
+              pl: 2,
+              pr: 2,
+              pt: 0.4,
+              pb: 0.4,
+            }}
+          >
+            <Typography sx={{ fontWeight: "bold" }}>
+              G {props.gusts} kts.
+            </Typography>
+          </Box>
+        )}
+      </Box>
+    </Box>
   );
 
   return (
     <>
       {typeof props.direction === "number" && props.direction > 0 && (
         <>
-          {compass}
-          <Box
-            sx={{
-              textAlign: "right",
-              color: "white",
-              border: 1,
-              borderColor: "primary.main",
-              borderRadius: 2,
-              ml: -1.3,
-              overflow: "hidden",
-            }}
-          >
-            <Box
-              sx={{
-                backgroundColor: "primary.main",
-                opacity: "60%",
-                pl: 2,
-                pr: 2,
-                pt: 0.4,
-                pb: 0.4,
-              }}
-            >
-              <Typography sx={{ fontWeight: "bold" }}>
-                {props.direction}°
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                backgroundColor: "primary.main",
-                opacity: "60%",
-                pl: 2,
-                pr: 2,
-                pt: 0.4,
-                pb: 0.4,
-              }}
-            >
-              <Typography sx={{ fontWeight: "bold" }}>
-                {props.speed} {props.unit}
-              </Typography>
-            </Box>
-            {props.gusts && (
-              <Box
-                sx={{
-                  backgroundColor: "warning.main",
-                  opacity: "60%",
-                  pl: 2,
-                  pr: 2,
-                  pt: 0.4,
-                  pb: 0.4,
-                }}
-              >
-                <Typography sx={{ fontWeight: "bold" }}>
-                  G {props.gusts} kts.
-                </Typography>
-              </Box>
-            )}
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            {compass}
+            <Typography textAlign="center" color="primary.main" marginTop={-8}>
+              select Runway
+            </Typography>
+            <Grid container>
+              {props.runways.map((el, key) => {
+                return (
+                  <Grid item key={key}>
+                    <Button
+                      sx={{ m: 1 }}
+                      onClick={() => setRunwayDirection(el.he_ident)}
+                      variant="contained"
+                    >
+                      RWY {el.he_ident}/{el.le_ident}
+                    </Button>
+                  </Grid>
+                );
+              })}
+            </Grid>
           </Box>
         </>
       )}
-      {props.direction === 0 && (
+      {props.direction === 0 && props.speed === 0 && (
         <Box
           sx={{
             textAlign: "center",
@@ -224,6 +299,23 @@ function Wind(props: IWind) {
             mt: 2,
             mb: 2,
             overflow: "hidden",
+            padding: 2,
+          }}
+        >
+          <Typography>no wind</Typography>
+        </Box>
+      )}
+      {props.direction === 0 && props.speed > 0 && (
+        <Box
+          sx={{
+            textAlign: "center",
+            border: 1,
+            borderColor: "primary.main",
+            borderRadius: 2,
+            mt: 2,
+            mb: 2,
+            overflow: "hidden",
+            padding: 2,
           }}
         >
           <Typography>
@@ -231,20 +323,6 @@ function Wind(props: IWind) {
           </Typography>
         </Box>
       )}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        {props.runways.map((el, key) => {
-          return (
-            <Button key={key} onClick={() => setRunwayDirection(el.he_ident)}>
-              RWY {el.he_ident}/{el.le_ident}
-            </Button>
-          );
-        })}
-      </Box>
     </>
   );
 }
