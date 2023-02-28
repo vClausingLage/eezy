868d268db0df3e1e41bdc1808b5dcbc493c72f4a
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-02-28 20:19:19 +0100

    react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState, useMemo } from "react";
 
 import AppHeader from "./AppHeader";
+import AppFooter from "./AppFooter";
 import Metar from "./components/Metar";
 import Aircraft from "./components/Aircraft";
 import Map from "./components/Map";
+import RawMetar from "./components/Metar/components/RawMetar";
 
 import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material/";
@@ -11,20 +13,32 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import { getDesignTokens } from "./CSS/theme";
 
-import RawMetar from "./components/Metar/components/RawMetar";
-import AppFooter from "./AppFooter";
-
-// THEME https://mui.com/material-ui/customization/default-theme/
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("dark");
 
   const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Metar />}>
+        <Route path="aircraft" element={<Aircraft />} />
+        <Route path="map" element={<Map />} />
+      </Route>
+    )
+  );
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <RouterProvider router={router} />
         <Box>
           <AppHeader />
           <Metar />
--- client/src/AppHeader.tsx
+++ client/src/AppHeader.tsx
@@ -5,12 +5,38 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+
+import { NavLink } from "react-router-dom";
+
 function AppHeader() {
+  let activeStyle = {
+    textDecoration: "underline",
+  };
+
+  let activeClassName = "underline";
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar></Toolbar>
-      </AppBar>
+    <Box>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="metar"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Metar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="aircraft"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Aircraft
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </Box>
   );
 }
