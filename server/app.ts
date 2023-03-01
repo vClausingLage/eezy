fe23c7b314113f643c387dd01eea805ea49ae2d7
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-01 22:09:52 +0100

    oauth

--- server/app.ts
+++ server/app.ts
@@ -20,12 +20,17 @@ app.use(
   })
 );
 app.use(cors());
-app.use(helmet()); //! https://stackoverflow.com/questions/68607669/react-app-served-with-express-and-helmet-cannot-make-api-requests
+app.use(helmet());
 app.disable("x-powered-by");
 
 app.use("/api/aircraft", ac_router);
 app.use("/api/metar", awc_router);
 app.use("/api/airport", airportDB_router);
+
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth20";
+
+passport.use(new GoogleStrategy.Strategy());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
