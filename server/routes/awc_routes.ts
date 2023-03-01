431ae6f9301ae71931b812d5cd500998e6a4f84e
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-01 21:15:03 +0100

    AWC controllers

--- server/routes/awc_routes.ts
+++ server/routes/awc_routes.ts
@@ -1,35 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+
+import { getMetar, getRawMetar } from "../services/controllersAWC.js";
 
 export const awc_router = Router();
 
-awc_router.get("/:metarID", async (req: Request, res: Response) => {
-  const icao = req.params.metarID;
-  try {
-    const fetchMetar = await fetch(
-      `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=$${icao}&format=json`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await fetchMetar.json();
-    if (fetchMetar.status === 200 && data.length > 0) res.send(data[0]);
-    else res.send({ message: "error" });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-awc_router.get("/raw/:metarID", async (req: Request, res: Response) => {
-  const icao = req.params.metarID;
-
-  const fetchRawMetar = await fetch(
-    `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=${icao}`
-  );
-  const data = await fetchRawMetar.text();
-  // const response = fetchMetar.headers;
-  if (data !== undefined) res.send(data);
-  else res.send({ message: "no data" });
-});
+awc_router.get("/:metarID", getMetar);
+awc_router.get("/raw/:metarID", getRawMetar);
---
+++ server/services/controllersAWC.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+
+export async function getMetar(req: Request, res: Response) {
+  const icao = req.params.metarID;
+  try {
+    const fetchMetar = await fetch(
+      `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=$${icao}&format=json`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await fetchMetar.json();
+    if (fetchMetar.status === 200 && data.length > 0) res.send(data[0]);
+    else res.send({ message: "error" });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getRawMetar(req: Request, res: Response) {
+  const icao = req.params.metarID;
+
+  const fetchRawMetar = await fetch(
+    `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=${icao}`
+  );
+  const data = await fetchRawMetar.text();
+  // const response = fetchMetar.headers;
+  if (data !== undefined) res.send(data);
+  else res.send({ message: "no data" });
+}
