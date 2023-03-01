30a9bd6e7f3840ff7658e504287052c142e8c853
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-01 20:37:06 +0100

    Wind.tsx

--- client/src/components/Metar/components/Wind.tsx
+++ client/src/components/Metar/components/Wind.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
