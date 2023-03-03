60843e825627e4e97b20f40244ec09a22483cb9f
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-03 16:14:00 +0100

    Aircraft init

--- client/src/components/Aircraft/index.tsx
+++ client/src/components/Aircraft/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -9,64 +9,16 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
 function Aircraft() {
-  // fetch("/aircraft/aircraft", {
-  //   method: "GET",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Accept: "application/json",
-  //   },
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data));
-
-  const [aircraftSelect, setAircraftSelect] = useState("");
-  const [aircraftModelSelect, setAircraftModelSelect] = useState("");
-
-  const handleACChange = (event: any) => {
-    // ! remove any
-    setAircraftSelect(event.currentTarget.value);
-  };
-  const handleModelChange = (event: any) => {
-    // ! remove any
-    setAircraftModelSelect(event.currentTarget.value);
-  };
+  useEffect(() => {
+    fetch("/api/aircraft");
+  });
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h2">Aircraft</Typography>
-        <Box sx={{ margin: "auto", minWidth: 120, maxWidth: "80%" }}>
-          <FormControl fullWidth>
-            <InputLabel id="ac-select-label">Manufacturer</InputLabel>
-            <Select
-              labelId="ac-select-label"
-              id="ac-select"
-              value={aircraftSelect}
-              label="Select Aircraft"
-              onChange={handleACChange}
-            >
-              <MenuItem value={10}>Cessna</MenuItem>
-              <MenuItem value={20}>Beechcraft</MenuItem>
-              <MenuItem value={30}>Piper</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel id="ac-model-select-label">Model</InputLabel>
-            <Select
-              labelId="ac-model-select-label"
-              id="ac-model-select"
-              value={aircraftModelSelect}
-              label="Select Aircraft Model"
-              onChange={handleModelChange}
-            >
-              <MenuItem value={10}>Cessna</MenuItem>
-              <MenuItem value={20}>Beechcraft</MenuItem>
-              <MenuItem value={30}>Piper</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      </CardContent>
-    </Card>
+    <>
+      <Card>
+        <CardContent></CardContent>
+      </Card>
+    </>
   );
 }
 
