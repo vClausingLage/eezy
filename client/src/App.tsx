53543b5f59eeb2106944780c9a6dbc8cab35a028
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-05 19:53:21 +0100

    authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 import "./CSS/App.css";
