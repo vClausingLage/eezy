ad1d836843a604995a34f1993e512cc560a852cc
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-09 18:43:38 +0100

    routes fix

--- server/routes/ac_routes.ts
+++ server/routes/ac_routes.ts
@@ -11,7 +11,7 @@ import {
 export const ac_router = Router();
 
 ac_router.get("/models", queryAircraftAll);
-ac_router.get("/:manufacturer", queryAircraftManufacturer);
+ac_router.get("/manufacturer/:manufacturer", queryAircraftManufacturer);
 ac_router.post("/create", createAircraft);
 ac_router.get("/create/:id", getAircraft);
 ac_router.post("/modelinsert", insertModelAC);
