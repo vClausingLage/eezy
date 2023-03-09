df5f7c402aa017cd61652fa10fde362dc33473cb
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-09 20:06:04 +0100

    Metar API init

--- server/routes/metar_api.routes.ts
+++ server/routes/metar_api.routes.ts
@@ -4,5 +4,6 @@ import { decodeRawMetar } from "../services/metar_api.controller.js";
 
 export const metar_api_router = Router();
 
-metar_api_router.get("/:metarstring", decodeRawMetar);
+metar_api_router.get("/:metarstring(*)", decodeRawMetar);
 // /api/metardecoder/:metarstring
+// localhost:4000/api/metardecoder/metarstring
--- server/services/metar_api.controller.ts
+++ server/services/metar_api.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 
 export async function decodeRawMetar(req: Request, res: Response) {
+  let metarString = req.params.metarstring;
   console.log("raw metar");
+  console.log(req.params.metarstring);
+  res.send({ input: metarString });
 }
