8d883a5ce0d8c037123f70cef6d570e07dd4e765
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2023-03-13 11:03:57 +0100

    refactor TextInput & Search Handling

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -7,67 +7,53 @@ import {
   TextInput,
   Button,
 } from "react-native";
-import { useState } from "react";
-
-import {
-  prepareMetar,
-  reduceTempo,
-  maptoMetarObj,
-} from "./helpers/metar-regex";
-import { IMetar } from "./helpers/IMetar";
+import { useState, useEffect } from "react";
 
 export default function App() {
   const [icao, setIcao] = useState("");
-  const [metarCode, setMetarCode] = useState<IMetar>();
-  const [gafor, setGafor] = useState("");
+  const [metarString, setMetarString] = useState("");
+  const [query, setQuery] = useState("");
+  const [alert, setAlert] = useState(false);
 
-  const searchIcao = async () => {
-    const fetchMetar = await fetch(
-      "https://api.met.no/weatherapi/tafmetar/1.0/metar?icao=" + icao
-    );
-    const data = await fetchMetar.text();
-    let metarList = prepareMetar(data);
-    let metarListReduced: string[][] = reduceTempo(metarList);
-    let metarObj = maptoMetarObj(metarListReduced[0]);
-    setMetarCode(metarObj);
-    setGafor(metarObj.GAFOR);
-  };
+  function searchIcao() {
+    setIcao(query);
+    console.log("search");
+  }
 
-  const clouds = () => {
-    if (metarCode?.Visibility === "CAVOK") {
-      return "☼";
-    } else if (metarCode?.Cloud_Layer[0].cloudLayer === "FEW") {
-      return "☁";
-    } else if (metarCode?.Cloud_Layer[0].cloudLayer === "SCT") {
-      return "☁☁";
-    } else if (metarCode?.Cloud_Layer[0].cloudLayer === "BKN") {
-      return "☁☁☁";
-    } else if (metarCode?.Cloud_Layer[0].cloudLayer === "OVC") {
-      return "☁☁☁☁";
-    } else {
-      return "no cloud conclusion";
+  useEffect(() => {
+    console.log("useEffect");
+    async function fetchMetar() {
+      const response = await fetch(
+        `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=${query}`
+      );
+      const data = await response.text();
+      setMetarString(data);
+      console.log(data);
     }
-  };
+    if (query.length === 4) fetchMetar();
+    else setAlert(true);
+  }, [icao]);
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
       <TextInput
         style={styles.input}
-        onChangeText={(input) => setIcao(input)}
-        value={icao}
+        onChangeText={(el) => setQuery(el)}
+        value={query}
         placeholder="Airport ICAO Code"
         keyboardType="default"
       />
+      {alert && <Text>provide valid ICAO Code</Text>}
       <Button
         onPress={searchIcao}
         title="search"
         color="#841584"
         accessibilityLabel="button search ICAO Code"
       />
-      <Text>QNH is {metarCode?.QNH}</Text>
-      <Text>Flight Rules: {gafor}</Text>
-      <Text>Cloud Layer: {clouds()}</Text>
+      <Text>{query}</Text>
+      <Text>{icao}</Text>
+      <Text>{metarString}</Text>
     </SafeAreaView>
   );
 }
@@ -76,13 +62,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
     paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
   },
   input: {
     height: 40,
-    margin: 12,
+    marginLeft: 25,
+    marginRight: 25,
+    marginBottom: 5,
     borderWidth: 1,
     padding: 10,
   },
