ca0fa3384b6164ddc31bd43bc09be9f338d3cacb
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-27 09:28:54 +0200

    redux aircraft update

--- client/src/features/redux/selectedAircraftSlice.ts
+++ client/src/features/redux/selectedAircraftSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const aircraftSlice = createSlice({
-  name: "aircraft",
+  name: "selectedAircraft",
   initialState: {
     object: {},
   },
