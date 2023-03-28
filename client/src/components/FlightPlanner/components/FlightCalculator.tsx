fb1b0636082703c78be899380d6e61c1c79bd30e
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-28 22:23:38 +0200

    fuel calculator Warning added

--- client/src/components/FlightPlanner/components/FlightCalculator.tsx
+++ client/src/components/FlightPlanner/components/FlightCalculator.tsx
@@ -3,9 +3,12 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import Alert from "@mui/material/Alert";
 import InputAdornment from "@mui/material/InputAdornment/InputAdornment";
+import Stack from "@mui/material/Stack";
+import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
 
-import { getRange, getDuration } from "../helper/fuel-calculator";
+import { getRange } from "../helper/fuel-calculator";
 
 import "../CSS/planner-calculator.css";
 
@@ -24,11 +27,14 @@ function FlightCalculator(props: Props) {
 
   function handleFuelChange(e: any) {
     if (
-      e.target.value !== undefined ||
-      e.target.value > 0 ||
-      e.target.value < props.fuelCapacity
-    )
+      e.target.value !== undefined &&
+      e.target.value >= 0
+      // && e.target.value <= props.fuelCapacity
+    ) {
       setFuelLoaded(parseInt(e.target.value));
+      setFuelMaxAlert(false);
+    }
+    if (e.target.value > props.fuelCapacity) setFuelMaxAlert(true);
   }
   function handleReserveChange(e: any) {
     setFuleReserve(e.target.value);
@@ -39,7 +45,12 @@ function FlightCalculator(props: Props) {
       <Typography variant="h5" color="primary.main">
         Fuel Calculator
       </Typography>
-      <Box className="fuel-selection">
+      <Stack
+        direction="row"
+        alignItems="center"
+        gap={1}
+        className="fuel-selection"
+      >
         <TextField
           label="Fuel Loaded"
           type="number"
@@ -51,43 +62,66 @@ function FlightCalculator(props: Props) {
             ),
           }}
           onChange={(e) => handleFuelChange(e)}
+          sx={{ width: 190 }}
         ></TextField>
-
         <Typography display="inline">
           of {props.fuelCapacity} liters max. Capacity
         </Typography>
-      </Box>
-      <TextField
-        label="Fuel Reserve"
-        type="number"
-        required
-        value={fuelReserve}
-        InputProps={{
-          endAdornment: <InputAdornment position="end">minutes</InputAdornment>,
-        }}
-        onChange={(e) => handleReserveChange(e)}
-      ></TextField>
+      </Stack>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <TextField
+          label="Fuel Reserve"
+          type="number"
+          required
+          value={fuelReserve}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">minutes</InputAdornment>
+            ),
+          }}
+          onChange={(e) => handleReserveChange(e)}
+          sx={{ width: 190 }}
+        ></TextField>
+        <Typography display="inline">
+          (equals {props.fuelConsumption * 0.75})
+        </Typography>
+      </Stack>
       <Box>
+        {fuelMaxAlert && (
+          <Alert severity="error">
+            Fuel Load exceeds your Aircraft&#700;s max. Capacity!
+          </Alert>
+        )}
         <Typography>
           max Range:{" "}
           {getRange(fuelLoaded, props.fuelConsumption, props.cruiseSpeed)}{" "}
           nautical miles
         </Typography>
       </Box>
-      <p>expected Taxi Fuel (default 5L)</p>
-      <p>headwinds -- use GS for fuel calc!</p>
-      <p>absolutly NECCESSARY</p>
-      <ul>
-        <li>monitor fuel consumption of previous flights</li>
-        <li>check fuel LOW indication</li>
-        <li>always apply fuel mixture</li>
-        <li>
-          https://www.simcoders.com/2018/09/07/how-to-calculate-the-fuel-required-for-your-flight/
-        </li>
-        <li>
-          https://www.flight-study.com/2021/05/basic-calculations-cross-country-flight.html
-        </li>
-      </ul>
+      <Typography className="attention-bar">
+        <Stack direction="row" alignItems="center" gap={1}>
+          <PriorityHighIcon /> ATTENTION
+        </Stack>
+        <ul>
+          <li>fuel calculations are guidelines not exact values</li>
+          <li>
+            always check your aircraft&#700;s flight manual, fuel consumption
+            may vary for individual aircraft
+          </li>
+          <li>
+            monitor your aircraft&#700;s fuel consumption of previous flights
+          </li>
+          <li>
+            note that most GA aircraft&#700;s fuel inidcators are vague and do
+            not indicate exact fuel amounts
+          </li>
+          <li>
+            apply mixture according to altitude and fly at the
+            manufacturer&#700;s recommended RPM
+          </li>
+          <li>double check with your own calculations</li>
+        </ul>
+      </Typography>
     </Box>
   );
 }
--- client/src/components/FlightPlanner/helper/fuel-calculator.ts
+++ client/src/components/FlightPlanner/helper/fuel-calculator.ts
@@ -6,5 +6,21 @@ export function getRange(
   fuelConsumption: number,
   cruiseSpeed: number
 ) {
-  return getDuration(fuelLoad, fuelConsumption) * cruiseSpeed;
+  return Math.round(getDuration(fuelLoad, fuelConsumption) * cruiseSpeed);
 }
+// function ktsToKmh(input: number): number {
+//   return input * 1.852;
+// }
+// function kmhToKts(input: number): number {
+//   return input / 1.852;
+// }
+// function litersToGallons(input: number): number {
+//   return input / 3.785;
+// }
+// function gallonsToLiters(input: number): number {
+//   return input * 3.785;
+// }
+
+// https://www.flight-study.com/2021/05/basic-calculations-cross-country-flight.html
+
+// https://www.simcoders.com/2018/09/07/how-to-calculate-the-fuel-required-for-your-flight/
