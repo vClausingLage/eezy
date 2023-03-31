40582cdc49efcb2abdbdf0fcf416fa4bae80dd85
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-03-31 10:51:26 +0200

    precip decoder update

--- client/src/components/Metar/helper/metar-helper-functions.ts
+++ client/src/components/Metar/helper/metar-helper-functions.ts
@@ -50,10 +50,37 @@ export function windVarFormat(windVar: string) {
   return output;
 }
 
-export function precipFormat(weatherString: string) {
+// export function precipFormatOld(weatherString: string) {
+//   let result: any = [];
+//   let output = [];
+//   weatherString = weatherString.replace(/\s/gi, "");
+//   while (weatherString.length > 0) {
+//     if (weatherString[0] === "-" || weatherString[0] === "+") {
+//       weatherString[0] === "-"
+//         ? (result = [...result, ["light", weatherString[1] + weatherString[2]]])
+//         : (result = [
+//             ...result,
+//             ["heavy", weatherString[1] + weatherString[2]],
+//           ]);
+//       weatherString = weatherString.slice(3);
+//     } else if (weatherString[0] !== "-" && weatherString[0] !== "+") {
+//       result = [...result, ["", weatherString[0] + weatherString[1]]];
+//       weatherString = weatherString.slice(2);
+//     }
+//   }
+//   for (let el of result) {
+//     for (const [key, value] of Object.entries(weatherCodes.characteristic)) {
+//       if (el[1] === key) output.push(el[0] + " " + value);
+//     }
+//     for (const [key, value] of Object.entries(weatherCodes.type)) {
+//       if (el[1] === key) output.push(el[0] + " " + value);
+//     }
+//   }
+//   return output.join(" and ");
+// }
+export function precipFormat(weatherString: string): string {
   let result: any = [];
   let output = [];
-  weatherString = weatherString.replace(/\s/gi, "");
   while (weatherString.length > 0) {
     if (weatherString[0] === "-" || weatherString[0] === "+") {
       weatherString[0] === "-"
@@ -76,66 +103,8 @@ export function precipFormat(weatherString: string) {
       if (el[1] === key) output.push(el[0] + " " + value);
     }
   }
-  return output.join(" and ");
+  return output.join("").trim();
 }
-
-// export function precipPreposition(precip: string) {
-//   let formattedPrecip: string[] = []
-//   if (precip.length % 2 === 0) {
-//     formattedPrecip = ['', precip];
-//   }
-//   else if (precip.length % 2 !== 0) {
-//     let preposition: string
-//     if (precip.slice(0, 1) === '-') {
-//       preposition = 'light'
-//     } else if (precip.slice(0, 1) === '+') {
-//       preposition = 'heavy'
-//     }
-//     formattedPrecip = [preposition!, precip.slice(1, precip.length)];
-//   }
-//   return formattedPrecip;
-// }
-
-// export function decodeWeather(precip: string[]) {
-//   let output: string[] = []
-//   // load JSON weather codes to [VAR]
-//   let codes = weatherCodes
-//   let codeArr: Array<Array<string>> = []
-//   for (const [k, v] of Object.entries(codes)) {
-//     for (const [code, descr] of Object.entries(v)) {
-//       codeArr.push([code, descr])
-//     }
-//   }
-//   // use VAR to LOOP METAR input
-//   let result: string[] = []
-//   if (precip[1].length >= 2) {
-//     for (let i = 0, charsLength = precip[1].length; i < charsLength; i += 2) {
-//       result.push(precip[1].substring(i, i + 2));
-//     }
-//   } else {
-//     console.log('precipitation code not properly formatted')
-//     // result.push(precip[1])
-//   }
-//   // check RPECIP for WEATHER CODES MATCH
-//   result.forEach((el) => {
-//     codeArr.forEach(x => {
-//       if (x[0] === el) {      // check weather code match x[0]
-//         output.push(x[1])     // push description of code x[1]
-//       }
-//     })
-//   })
-//   return output
-// }
-
-// export function precipFormat(precip: string) {
-//   precip = precip.replaceAll(' ', '')
-//   let output = new Precipitation();
-//   let newPrecip = precipPreposition(precip);
-//   let weatherCode = decodeWeather(newPrecip);
-//   output.intensity = newPrecip[0];
-//   output.elements = weatherCode;
-//   return output;
-// }
 
 export function cloudFormat(clouds: string) {
   let output = new Clouds();
