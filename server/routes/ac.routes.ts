893a4f0bb8d2e6be60e13dc178d6584e8cabb396
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2023-04-22 21:51:19 +0200

    mongoose and MongoDB replaced with sequelize, SQL

--- server/routes/ac.routes.ts
+++ server/routes/ac.routes.ts
@@ -3,15 +3,9 @@ import { Router } from "express";
 import {
   createAircraft,
   getAircraft,
-  // queryAircraftManufacturer,
-  // insertModelAC,
-  // queryAircraftAll,
 } from "../services/aircraft.sequelize.controller.js";
 
 export const ac_router = Router();
 
 ac_router.post("/create", createAircraft);
 ac_router.get("/create/:id", getAircraft);
-// ac_router.get("/models", queryAircraftAll);
-// ac_router.get("/manufacturer/:manufacturer", queryAircraftManufacturer);
-// ac_router.post("/modelinsert", insertModelAC);
--- server/routes/metar_api.routes.ts
+++ server/routes/metar_api.routes.ts
@@ -5,5 +5,3 @@ import { decodeRawMetar } from "../services/metar_api.controller.js";
 export const metar_api_router = Router();
 
 metar_api_router.get("/:metarstring(*)", decodeRawMetar);
-// /api/metardecoder/:metarstring
-// localhost:4000/api/metardecoder/metarstring
