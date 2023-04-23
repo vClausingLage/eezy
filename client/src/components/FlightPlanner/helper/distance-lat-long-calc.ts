c435fd5e7671bab8d356cd60806483d631d450e7
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-04-23 10:04:48 +0200

    fuel calculation and route calculation added

--- client/src/components/FlightPlanner/helper/distance-lat-long-calc.ts
+++ client/src/components/FlightPlanner/helper/distance-lat-long-calc.ts
@@ -17,3 +17,20 @@ export function calcLatLong(latLong: latLong[]): number {
   // latLong = latLong.replaceAll(' ', '').replaceAll(`°`, '').replaceAll(`'`, '').replaceAll(`"`, '').replaceAll(`''`, '')
   return distance;
 }
+
+// https://www.movable-type.co.uk/scripts/latlong.html
+
+// JavaScript:
+
+// const R = 6371e3; // metres
+// const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+// const φ2 = lat2 * Math.PI/180;
+// const Δφ = (lat2-lat1) * Math.PI/180;
+// const Δλ = (lon2-lon1) * Math.PI/180;
+
+// const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+//           Math.cos(φ1) * Math.cos(φ2) *
+//           Math.sin(Δλ/2) * Math.sin(Δλ/2);
+// const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+// const d = R * c; // in metres
