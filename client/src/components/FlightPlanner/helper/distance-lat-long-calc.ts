9d8d0d98e6a6fe360d37fe92d9da7cd0c7fa0fa3
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-04-23 10:43:58 +0200

    routeCalc formula update

--- client/src/components/FlightPlanner/helper/distance-lat-long-calc.ts
+++ client/src/components/FlightPlanner/helper/distance-lat-long-calc.ts
@@ -6,13 +6,30 @@ type latLong = {
 
 export function calcLatLong(latLong: latLong[]): number {
   console.log("latLong", latLong);
-  let distance: number =
-    Math.acos(
-      Math.sin(latLong[0].latitude) * Math.sin(latLong[1].latitude) +
-        Math.cos(latLong[0].latitude) *
-          Math.cos(latLong[1].latitude) *
-          Math.cos(latLong[1].longitude - latLong[0].longitude)
-    ) * 6371;
+  // let distance: number =
+  //   Math.acos(
+  //     Math.sin(latLong[0].latitude) * Math.sin(latLong[1].latitude) +
+  //       Math.cos(latLong[0].latitude) *
+  //         Math.cos(latLong[1].latitude) *
+  //         Math.cos(latLong[1].longitude - latLong[0].longitude)
+  //   ) * 6371;
+
+  const R = 6371e3;
+  const phi1 = (latLong[0].latitude * Math.PI) / 180;
+  const phi2 = (latLong[1].latitude * Math.PI) / 180;
+  const DeltaPhi =
+    ((latLong[1].latitude - latLong[0].latitude) * Math.PI) / 180;
+  const DeltaLambda =
+    latLong[1].longitude - (latLong[0].longitude * Math.PI) / 180;
+
+  const a =
+    Math.sin(DeltaPhi / 2) * Math.sin(DeltaPhi / 2) +
+    Math.cos(phi1) *
+      Math.cos(phi2) *
+      Math.sin(DeltaLambda / 2) *
+      Math.sin(DeltaLambda / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
   console.log("distance", distance);
   // latLong = latLong.replaceAll(' ', '').replaceAll(`°`, '').replaceAll(`'`, '').replaceAll(`"`, '').replaceAll(`''`, '')
   return distance;
@@ -23,10 +40,10 @@ export function calcLatLong(latLong: latLong[]): number {
 // JavaScript:
 
 // const R = 6371e3; // metres
-// const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-// const φ2 = lat2 * Math.PI/180;
-// const Δφ = (lat2-lat1) * Math.PI/180;
-// const Δλ = (lon2-lon1) * Math.PI/180;
+// const φ1 = lat0 * Math.PI/180; // φ, λ in radians
+// const φ2 = lat1 * Math.PI/180;
+// const Δφ = (lat1-lat0) * Math.PI/180;
+// const Δλ = (lon1-lon0) * Math.PI/180;
 
 // const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
 //           Math.cos(φ1) * Math.cos(φ2) *
