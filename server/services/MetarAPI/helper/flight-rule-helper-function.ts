6b34b9f668ac9882552115bf62c7fe61b85dd708
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2023-05-05 09:57:25 +0200

    SeaMiles added to flightRuleCalculator

--- server/services/MetarAPI/helper/flight-rule-helper-function.ts
+++ server/services/MetarAPI/helper/flight-rule-helper-function.ts
@@ -44,7 +44,6 @@ function metersFLR(
     return { flight_rule: "incomplete data", color_code: "black" };
   }
 }
-//! add nm data for FLR
 function nauticalMilesFLR(
   visibility: { value: number | string | undefined; unit: string },
   cloud_base: number | undefined
@@ -53,22 +52,22 @@ function nauticalMilesFLR(
   if (typeof visibility === "string" && visibility === "CAVOK") {
     return { flight_rule: "VFR", color_code: "green" };
   } else if (
-    (typeof visibility === "number" && visibility <= 1500) ||
+    (typeof visibility === "number" && visibility <= 1) ||
     cloud_base * 100 <= 500
   ) {
     return { flight_rule: "LIFR", color_code: "purple" };
   } else if (
-    (typeof visibility === "number" && visibility <= 5000) ||
+    (typeof visibility === "number" && visibility <= 3) ||
     cloud_base * 100 <= 1000
   ) {
     return { flight_rule: "IFR", color_code: "red" };
   } else if (
-    (typeof visibility === "number" && visibility <= 8000) ||
+    (typeof visibility === "number" && visibility <= 5) ||
     cloud_base * 100 <= 3000
   ) {
     return { flight_rule: "MVFR", color_code: "blue" };
   } else if (
-    (typeof visibility === "number" && visibility > 2) ||
+    (typeof visibility === "number" && visibility > 5) ||
     cloud_base * 100 > 3000
   ) {
     return { flight_rule: "VFR", color_code: "green" };
