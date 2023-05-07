545858c8c94d06577f2922f276de638ca0eebb18
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2023-05-07 19:22:11 +0200

    formulas: comments removed

--- client/src/pages/FlightPlanner/helper/distance-lat-long-calc.ts
+++ client/src/pages/FlightPlanner/helper/distance-lat-long-calc.ts
@@ -12,34 +12,6 @@ export function calcLatLong(latLong: latLong[]): number {
   const lon0 = latLong[0].longitude;
   const lon1 = latLong[1].longitude;
 
-  // let distance: number =
-  //   Math.acos(
-  //     Math.sin(latLong[0].latitude) * Math.sin(latLong[1].latitude) +
-  //       Math.cos(latLong[0].latitude) *
-  //         Math.cos(latLong[1].latitude) *
-  //         Math.cos(latLong[1].longitude - latLong[0].longitude)
-  //   ) * 6371;
-  // --------------------------------------
-  // const R = 6371e3;
-  // const phi1 = (latLong[0].latitude * Math.PI) / 180;
-  // const phi2 = (latLong[1].latitude * Math.PI) / 180;
-  // const DeltaPhi =
-  //   ((latLong[1].latitude - latLong[0].latitude) * Math.PI) / 180;
-  // const DeltaLambda =
-  //   latLong[1].longitude - (latLong[0].longitude * Math.PI) / 180;
-
-  // const a =
-  //   Math.sin(DeltaPhi / 2) * Math.sin(DeltaPhi / 2) +
-  //   Math.cos(phi1) *
-  //     Math.cos(phi2) *
-  //     Math.sin(DeltaLambda / 2) *
-  //     Math.sin(DeltaLambda / 2);
-  // const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  // const distance = R * c;
-  // console.log("distance", distance);
-
-  // latLong = latLong.replaceAll(' ', '').replaceAll(`°`, '').replaceAll(`'`, '').replaceAll(`"`, '').replaceAll(`''`, '')
-
   const R = 6371e3; // metres
   const φ1 = (lat0 * Math.PI) / 180; // φ, λ in radians
   const φ2 = (lat1 * Math.PI) / 180;
