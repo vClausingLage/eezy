e30dac7b472c945cca887e4c56122f9d97fe38a2
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-05-12 13:22:08 +0200

    client auth0 removed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -18,19 +18,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <CssBaseline enableColorScheme />
-    {/* <Auth0Provider
-      domain={Auth0Domain}
-      clientId={Auth0ClientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    > */}
+
     <App />
-    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
