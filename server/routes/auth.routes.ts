8e8aa1a1625aee24789a2e00823f859a65213d05
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-05-23 12:33:57 +0200

    auth routes

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,5 +1,5 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 export const auth_router = Router();
 
-auth_router.get("/login", () => console.log("login"));
+auth_router.get("/login", (req: Request, res: Response) => res.send("login"));
