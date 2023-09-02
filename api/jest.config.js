2a2922fd0bd860637af3f397572945911141315e
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-09-02 13:40:46 +0200

    ADD tests with jest

---
+++ api/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  transform: { '^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  }
+}
--- api/services/MetarAPI/tests/metar-regex-main.test.ts
+++ api/services/MetarAPI/tests/metar-regex-main.test.ts
@@ -1,22 +1,48 @@
 import test, { describe } from "node:test";
+
 import { metarDecoder } from "../helper/metar-regex-main.js";
 
 describe("metarDecoder", () => {
   test("schould return a metar object", () => {
-    expect(metarDecoder("")).toEqual({
-      metar: "",
-      station: "",
-      time: "",
-      wind: { direction: 0, speed: 0, unit: "kts" },
-      visibility: 0,
-      runwayVisualRange: [],
-      weather: [],
-      clouds: [],
-      temperature: 0,
-      dewPoint: 0,
-      altimeter: 0,
-      remarks: "",
-      color: "",
+    expect(metarDecoder("EDHK 020950Z 29007KT 9999 FEW027 19/13 Q1020")).toEqual({
+      icao: "EDHK",
+      date: "2023-09-02T09:50:00.000Z",
+      cavok: false,
+      nosig: false,
+      auto: false,
+      air_pressure: {
+        pressure: "QNH",
+        value: 1020,
+        unit: "hPa"
+      },
+      clouds: [
+        {
+          cloud_layer: "FEW",
+          cloud_base: 27,
+        }
+      ],
+      wind: {
+        direction: 290,
+        speed: 7,
+        unit: "kts",
+      },
+      temperature: {
+        temp: 19,
+        dewp: 13,
+      },
+      visibility: {
+        value: 9999,
+        unit: "m",
+      },
+      precepitation: [
+      ],
+      recent: [
+      ],
+      becoming: [
+      ],
+      tempo: [
+      ],
+      raw_metar: "EDHK 020950Z 29007KT 9999 FEW027 19/13 Q1020"
     });
   });
 });
