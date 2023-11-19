1eddba29b610fdc89f5433bba1769e1ed348f933
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2023-11-19 15:28:34 +0100

    MOD API url change AWC

--- api/services/awc.controller.ts
+++ api/services/awc.controller.ts
@@ -2,9 +2,12 @@ import { Request, Response } from 'express'
 
 export async function getMetar(req: Request, res: Response) {
   const icao = req.params.metarID
+  const date = new Date()
+  const dateString = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}_${date.getHours()}${date.getMinutes()}`
+  const url = `https://aviationweather.gov/api/data/metar?ids=${icao}&date=${dateString}&format=json` //! Date must beu Zulu
   try {
     const fetchMetar = await fetch(
-      `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=$${icao}&format=json`,
+      url,
       {
         method: 'get',
         headers: {
@@ -13,6 +16,7 @@ export async function getMetar(req: Request, res: Response) {
       }
     )
     const data = await fetchMetar.json()
+    console.log(data)
     if (fetchMetar.status === 200 && data.length > 0) res.send(data[0])
     else res.send({ message: 'error' })
   } catch (error) {
@@ -23,7 +27,7 @@ export async function getMetar(req: Request, res: Response) {
 export async function getRawMetar(req: Request, res: Response) {
   const icao = req.params.metarID
   const fetchRawMetar = await fetch(
-    `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=${icao}`
+    `https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids=${icao}` //! change url
   )
   const data = await fetchRawMetar.text()
   // const response = fetchMetar.headers;
